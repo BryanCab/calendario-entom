68f10ce64fc762647aee46baffbeab5359602df7
BryanCab <holkercabrera11@gmail.com>
2026-02-11 10:45:37 -0600

    v4 service worker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // ⚡ Nueva versión — incrementar cuando cambien archivos
-const CACHE_VERSION = "v3";
+const CACHE_VERSION = "v4";
 const CACHE_NAME = `calendario-entom-${CACHE_VERSION}`;
 
 const ASSETS = [
